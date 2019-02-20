e6f253d6baf8deb0264a43a3dceb52ffa6af953a
Alberto M <mad.hh.pwn@gmail.com>
2019-02-20 10:21:36 +0100

    dosdevuelvedos

--- test/com/cifpcarballeira/katas/FizzBuzz/Tests/TestFizzBuzz.java
+++ test/com/cifpcarballeira/katas/FizzBuzz/Tests/TestFizzBuzz.java
@@ -25,4 +25,11 @@ public class TestFizzBuzz {
         String resultado = fb.getFizzBuzz(1);
         assertEquals("1", resultado);
     }
+    
+    @Test
+    public void testdosesdos() {
+        FizzBuzz fb = new FizzBuzz();
+        String resultado = fb.getFizzBuzz(2);
+        assertEquals("2", resultado);
+    }
 }
