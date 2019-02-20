3e84e1c2ea91fcc7006df5f5c9a006813da13820
Alberto M <mad.hh.pwn@gmail.com>
2019-02-20 10:13:27 +0100

    unodevuelveuno

---
+++ src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cifpcarballeira.katas.FizzBuzz;
+
+/**
+ *
+ * @author dam220
+ */
+public class FizzBuzz {
+    public static void main(String[] args) {
+        
+    }
+    
+    public String getFizzBuzz(int i){
+        return i+"";
+    }
+}
